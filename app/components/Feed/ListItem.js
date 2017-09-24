836252b01fe5152081227c9fee246ee2fc0afbcc
Ivan Rudnev <rudnev.vanya@gmail.com>
2017-09-24 18:25:33 +0300

    Separated feed styles

--- app/components/Feed/ListItem.js
+++ app/components/Feed/ListItem.js
@@ -1,7 +1,43 @@
 import React from 'react';
-import { Text, View, Image } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 import { connect } from 'react-redux';
 import { Card, CardItem, Left, Right, Body, Icon } from 'native-base';
+
+const styles = StyleSheet.create({
+  avatar: {
+    width: 50,
+    height: 50,
+    borderRadius: 25,
+  },
+  header_name: {
+    fontSize: 13,
+    fontWeight: 'bold',
+  },
+  header_location: {
+    fontSize: 13,
+  },
+  options: {
+    flex: 0.1,
+  },
+  main_image: {
+    width: 350,
+    height: 300,
+    alignItems: 'center',
+  },
+  likes: {
+    flex: 0.25,
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  dots: {
+    flex: 0.5,
+    textAlign: 'center',
+  },
+  save: {
+    flex: 0.25,
+    textAlign: 'right',
+  },
+});
 
 // eslint-disable-next-line
 class ListItem extends React.Component {
@@ -9,39 +45,36 @@ class ListItem extends React.Component {
     return (
       <Card>
         <CardItem>
-          <Left style={{ flex: 0.8 }}>
+          <Left>
             <Image
-              style={{ width: 50, height: 50, borderRadius: 25 }}
+              style={styles.avatar}
               source={{ uri: 'https://goo.gl/EzYtKK' }}
             />
             <Body>
-              <Text style={{ fontWeight: 'bold', fontSize: 13 }}>Салон Jasmine. Мастер
-                            Евгения.</Text>
-              <Text note style={{ fontSize: 13 }}>Рядом с метро Курская</Text>
+              <Text style={styles.header_name}>Салон Jasmine. Мастер Евгения.</Text>
+              <Text style={styles.header_location}>Рядом с метро Курская</Text>
             </Body>
           </Left>
-          <Right style={{ flex: 0.2 }}>
+          <Right style={styles.options}>
             <Icon name="list" />
           </Right>
         </CardItem>
         <CardItem>
           <Image
-            style={{ width: 350, height: 300, alignItems: 'center' }}
+            style={styles.main_image}
             source={{ uri: 'https://github.com/ppsreejith.png?size=200' }}
           />
         </CardItem>
 
-        <CardItem style={{ flex: 1, justifyContent: 'space-between' }}>
-
-          <View style={{ flex: 0.25, flexDirection: 'row', alignItems: 'center' }}>
+        <CardItem>
+          <View style={styles.likes}>
             <Icon name="ios-heart" />
             <Text>1.99k</Text>
           </View>
 
-          <Text style={{ flex: 0.5, textAlign: 'center' }}>Точечки</Text>
+          <Text style={styles.dots}>Точечки</Text>
 
-          <Icon name="book" style={{ flex: 0.25, textAlign: 'right' }} />
-
+          <Icon name="book" style={styles.save} />
         </CardItem>
       </Card>
     );
