b062a2bcf1638938c53deac907541a718c660b25
e.utrobin <e.utrobin@corp.mail.ru>
2017-10-08 11:38:40 +0300

    Внес правки

--- app/store/createStore.js
+++ app/store/createStore.js
@@ -1,10 +1,13 @@
 import { createStore, applyMiddleware } from 'redux';
 import logger from 'redux-logger';
+import { composeWithDevTools } from 'redux-devtools-extension';
 import rootReducer from '../modules/index/index.reducer';
 
 export default function () {
   return createStore(
     rootReducer,
-    applyMiddleware(logger),
+    composeWithDevTools(
+      applyMiddleware(logger),
+    )
   );
 }
