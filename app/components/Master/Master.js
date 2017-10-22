31332c66a04651de90ad8938bff140e6e61f8ebb
Ivan Rudnev <rudnev.vanya@gmail.com>
2017-10-22 16:31:02 +0300

    Pretty master header

--- app/components/Master/Master.js
+++ app/components/Master/Master.js
@@ -15,13 +15,30 @@ class Master extends React.Component {
       <View style={styles.container}>
         <View style={styles.header}>
 
-            <Image style={styles.sign} source={{ uri: 'https://cdn3.iconfinder.com/data/icons/picons-social/57/80-google-plus-512.png' }} />
+          <Image style={styles.sign} source={{ uri: 'https://cdn3.iconfinder.com/data/icons/picons-social/57/80-google-plus-512.png' }} />
 
-            <Image
-              style={styles.avatar}
-              source={{ uri: 'http://cdn-img.instyle.com/sites/default/files/styles/276x276/public/images/2014/TRANSFORMATIONS/2009b-megan-fox-567_0.jpg?itok=Ex_Uv619' }}
-            />
-            <Text style={styles.name}>Megan</Text>
+          <Image
+            style={styles.avatar}
+            source={{ uri: 'http://cdn-img.instyle.com/sites/default/files/styles/276x276/public/images/2014/TRANSFORMATIONS/2009b-megan-fox-567_0.jpg?itok=Ex_Uv619' }}
+          />
+          <Text style={styles.name}>Megan</Text>
+
+          <View style={styles.statistic_container}>
+            <View style={styles.statistic}>
+              <Text style={styles.statistic_number}>72</Text>
+              <Text style={styles.statistic_caption}>Order</Text>
+            </View>
+
+            <View style={styles.statistic}>
+              <Text style={styles.statistic_number}>5950</Text>
+              <Text style={styles.statistic_caption}>Integral</Text>
+            </View>
+
+            <View style={styles.statistic}>
+              <Text style={styles.statistic_number}>18</Text>
+              <Text style={styles.statistic_caption}>Coupons</Text>
+            </View>
+          </View>
 
         </View>
       </View>
--- app/components/Master/Master.styles.js
+++ app/components/Master/Master.styles.js
@@ -18,21 +18,48 @@ const styles = StyleSheet.create({
   },
 
   sign: {
-    width: 26,
-    height: 26,
-    flex: 'right',
+    width: 30,
+    height: 30,
+    marginRight: 15,
+    marginTop: 10,
+    alignSelf: 'flex-end',
   },
 
   avatar: {
+    marginTop: 5,
     width: 80,
     height: 80,
     borderRadius: 40,
   },
 
   name: {
-    fontSize: 18,
-    marginTop: 10,
+    fontSize: 14,
+    marginTop: 5,
     color: '#ffffff',
+    fontWeight: 'bold',
+  },
+
+  statistic_container: {
+    marginTop: 30,
+    marginLeft: 35,
+    marginRight: 35,
+    flexDirection: 'row',
+  },
+
+  statistic: {
+    alignItems: 'center',
+    flex: 1,
+  },
+
+  statistic_number: {
+    color: '#ffffff',
+    fontSize: 22,
+    fontWeight: 'bold',
+  },
+
+  statistic_caption: {
+    color: '#c3c3c3',
+    fontSize: 14,
   },
 
   body: {
